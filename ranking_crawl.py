f9385143ec3beccdb09b3ae4db8806883df34473
unknown <rnjsguswo012@hufs.ac.kr>
2021-02-11 18:20:53 +0900

    Get products price

--- ranking_crawl.py
+++ ranking_crawl.py
@@ -8,8 +8,10 @@ f = open("test.txt", "w", encoding="utf-8")
 for pagenum in range(1, 101):
     driver.get("https://search.musinsa.com/ranking/best?period=now&mainCategory=&subCategory=&leafCategory=&price=&golf=false&newProduct=false&exclusive=false&discount=false&soldOut=false&page=%d&viewType=small&device=&priceMin=&priceMax=" % pagenum)
     titles = driver.find_elements_by_css_selector("p.list_info > a")
+    prices = driver.find_elements_by_css_selector("p.price")
     for i in range(len(titles)):
         f.write(str(titles[i].get_attribute("title")) + "\n")
+        f.write(str(prices[i].text) + "\n")
 
 
 #driver.get("http://www.python.org")
