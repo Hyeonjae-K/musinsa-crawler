01c9a5c1b83fe54ee379cfa96a8ca015e586bf11
unknown <rnjsguswo012@hufs.ac.kr>
2021-02-15 18:48:14 +0900

    Get server time

--- auto_purchase.py
+++ auto_purchase.py
@@ -4,7 +4,7 @@ from selenium.webdriver.common.keys import Keys
 
 productUrl = input("Enter the product url: ")
 # productSize = input("Enter the product size: ")
-# openTime = input("Enter the time by 24 hour system(hhmm): ")
+# openTime = int(input("Enter the time by 24 hour system(hhmm): "))
 
 timeDriver = webdriver.Chrome()
 timeDriver.get("https://time.navyism.com/")
@@ -13,3 +13,6 @@ inputBar = timeDriver.find_element_by_css_selector("#inputhere")
 inputBar.click()
 inputBar.send_keys(productUrl)
 inputBar.send_keys(Keys.ENTER)
+
+getTime = timeDriver.find_element_by_css_selector("#time_area").text
+getTime = int(getTime[getTime.find("일") + 2:getTime.find("분")].replace("시 ", ""))
