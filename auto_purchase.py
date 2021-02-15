ab17301cf85a84d17d84c3fe070605f42327e393
unknown <rnjsguswo012@hufs.ac.kr>
2021-02-15 19:22:00 +0900

    Set login time and break when login time

--- auto_purchase.py
+++ auto_purchase.py
@@ -4,7 +4,13 @@ from selenium.webdriver.common.keys import Keys
 
 productUrl = input("Enter the product url: ")
 # productSize = input("Enter the product size: ")
-# openTime = int(input("Enter the time by 24 hour system(hhmm): "))
+openTime = int(input("Enter the open time by 24 hour system(hhmm): "))
+loginTime = int(input("Enter the number of minutes to login(<60): "))
+
+if openTime % 100 < loginTime:
+    loginTime = openTime - 40 - loginTime
+else:
+    loginTime = openTime - loginTime
 
 timeDriver = webdriver.Chrome()
 timeDriver.get("https://time.navyism.com/")
@@ -14,5 +20,11 @@ inputBar.click()
 inputBar.send_keys(productUrl)
 inputBar.send_keys(Keys.ENTER)
 
-getTime = timeDriver.find_element_by_css_selector("#time_area").text
-getTime = int(getTime[getTime.find("일") + 2:getTime.find("분")].replace("시 ", ""))
+while True:
+    getTime = timeDriver.find_element_by_css_selector("#time_area").text
+    getTime = int(getTime[getTime.find("일") + 2:getTime.find("분")].replace("시 ", ""))
+    print(getTime)
+    if getTime >= loginTime:
+        break
+
+# timeDriver.close()
