06625a1b9b7a54da1d1e4ce8b99c2acb77be9b1a
unknown <rnjsguswo012@hufs.ac.kr>
2021-03-19 16:09:39 +0900

    Version 1

--- point_macro.py
+++ point_macro.py
@@ -1,14 +1,15 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+import time
 
 driver = webdriver.Chrome()
 driver.get("https://store.musinsa.com/app/")
+f = open('./info.txt', 'r', encoding='UTF-8')
 
 
 def login():
-    with open('./info.txt', 'r', encoding='UTF-8') as f:
-        my_id = f.readline().strip()
-        my_pw = f.readline().strip()
+    my_id = f.readline().strip()
+    my_pw = f.readline().strip()
 
     driver.find_element_by_css_selector("#default_top > div.header-member > button").click()
 
@@ -23,6 +24,44 @@ def login():
     driver.find_element_by_css_selector("body > div.bottom-column.column.clearfix.n-member-area > div > div.loginBoxV3 > form > span.submit.submitWBOX > input[type=submit]").click()
 
 
-login()
+def open_page():
+    driver.get("https://magazine.musinsa.com/index.php?m=magazine&uid=15890")
 
-driver.close()
+
+def run_macro():
+    comment = f.readline().strip()
+    nick = f.readline().strip()
+
+    while True:
+        while True:
+            try:
+                comment_input = driver.find_element_by_css_selector("#wrapper > div.bottom-column.column.clearfix > div.main-content-wrapper > div:nth-child(3) > div > div > div.replyBoard-box.box > div.postRight > div > div > ul > div.gWarea > form > div.cFormTriger > span")
+            except:
+                continue
+            else:
+                comment_input.click()
+                break
+
+        comment_input = driver.find_element_by_css_selector("#wrapper > div.bottom-column.column.clearfix > div.main-content-wrapper > div:nth-child(3) > div > div > div.replyBoard-box.box > div.postRight > div > div > ul > div.gWarea > form > div.cFormBox.groupType-cForm > div.cForm > textarea")
+        comment_input.click()
+        comment_input.send_keys(comment)
+
+        driver.find_element_by_css_selector("#wrapper > div.bottom-column.column.clearfix > div.main-content-wrapper > div:nth-child(3) > div > div > div.replyBoard-box.box > div.postRight > div > div > ul > div.gWarea > form > div.cFormBox.groupType-cForm > div.btnGroup > div > input.submit.mcmment-command-submit-comment").send_keys(Keys.ENTER)
+        driver.switch_to.alert.accept()
+        time.sleep(2)
+
+        driver.find_element_by_css_selector("#wrapper > div.bottom-column.column.clearfix > div.main-content-wrapper > div:nth-child(3) > div > div > div.replyBoard-box.box > div.postRight > div > div > ul > li:nth-child(5) > div.info > div.score.ui-require-all > a.ui-require-cool").click()
+
+        driver.find_element_by_css_selector("#wrapper > div.bottom-column.column.clearfix > div.main-content-wrapper > div:nth-child(3) > div > div > div.replyBoard-box.box > div.postRight > div > div > ul > li:nth-child(5) > div.info > label").click()
+        driver.find_element_by_css_selector("#wrapper > div.bottom-column.column.clearfix > div.main-content-wrapper > div:nth-child(3) > div > div > div.replyBoard-box.box > div.postRight > div > div > ul > li:nth-child(5) > div.info > label > div > a:nth-child(2)").click()
+        driver.switch_to.alert.accept()
+        driver.refresh()
+
+        time.sleep(7)
+
+
+login()
+open_page()
+run_macro()
+
+# driver.close()
